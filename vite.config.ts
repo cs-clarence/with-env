9b0781777144dee5efba836883ffa7d6da8fafae
Clarence Manuel <rencedm112@gmail.com>
2023-06-27 09:49:08 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,19 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default {
   plugins: [tsconfigPaths()],
+  optimizeDeps: {},
   build: {
     outDir: "dist",
+    rollupOptions: {
+      external: [
+        "os",
+        "path",
+        "crypto",
+        "node:child_process",
+        "node:path",
+        "node:fs",
+      ],
+    },
     lib: {
       entry: "./src/index.ts",
       formats: ["es", "cjs"],
